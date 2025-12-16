a32035ee04076e3056a15714819ee036ac23d579
sidm01890 <siddharth.mishra@corepeelers.com>
2025-12-16 17:11:59 +0530

    Fix: Don't override existing env vars from properties (allow docker-compose env vars to take precedence)

--- app/core/properties_loader.py
+++ app/core/properties_loader.py
@@ -138,7 +138,9 @@ class PropertiesLoader:
             # Also set mapped environment variable names (for Pydantic Settings)
             if key in property_to_env_map:
                 env_key = property_to_env_map[key]
-                if env_key not in os.environ:  # Don't override existing env vars
+                # Only set env var if not already set (don't override docker-compose env vars)
+                # This allows docker-compose env vars to take precedence over properties
+                if env_key not in os.environ:
                     os.environ[env_key] = value
         
         return self.properties
